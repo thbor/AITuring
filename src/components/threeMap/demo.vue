9707aff46260ae6d684c6a5cce028f38b7f18de8
bobo <875792791@qq.com>
2020-04-16 15:02:29 +0800

    封裝obj對象爲共有屬性

--- src/components/threeMap/demo.vue
+++ src/components/threeMap/demo.vue
@@ -8,7 +8,7 @@ import Test from './test'
  import * as THREE from "three";
   import {CSS2DRenderer, CSS2DObject} from 'three-css2drender';
 const OrbitControls = require('three-orbit-controls')(THREE);
-import {addObj} from '../../service/Manager'
+import {addObj,addObj2,addObj3} from '../../service/Manager'
 export default {
   components:{
     Test
@@ -16,7 +16,18 @@ export default {
   data(){
     return{
       scene:"",
-      cubic:""
+      cubic:"",
+      mesh1:{
+        path:'/static/model/street/',
+        mtl:"Street environment_V01.mtl",
+        loadobj:"Street environment_V01.obj",
+        scale:{x:0.8, y:0.8, z:0.8},
+        position:{x:-40, y:-50, z:10},
+        spriteInfo:[{x:-95,y:-55,z:-110,image:'/static/image/shoop.png',loc:-70,text:'SKECH'},
+        {x:-80,y:-65,z:-90,image:'/static/image/cloth.png',loc:-52,text:'FILA'},
+        {x:-100,y:-45,z:-80,image:'/static/image/apple.png',loc:-63,text:'APPLE'},]
+
+      }
     }
   },
   mounted(){
@@ -28,9 +39,12 @@ export default {
       this.scene.add(addObj(0,40,0))
       this.scene.add(addObj(40,0,0))
       this.scene.add(addObj(0,0,40))
+      // this.scene.add(addObj3(this.scene))
+      this.scene.add(addObj2(this.mesh1,this.scene))
+      // this.scene.add(addObj2())
     },
     sceneData(scene){
-      console.log("scene!!!",scene)
+      console.log("scene!1!!",scene)
       this.scene = scene
     },
     addCubic(){
--- src/service/Manager.js
+++ src/service/Manager.js
@@ -1,17 +1,49 @@
 import * as THREE from "three";
-export const setObjDemo=(data,demoObj1)=>{
-  new MTLLoader().setPath(demoObj1.path).load(demoObj1.load, materials => {
-    console.log("materials", materials);
-    materials.preload();
-    new OBJLoader().setMaterials(materials).setPath(demoObj1.path).load(demoObj1.dataLoadObj, obj => {
-      obj.scale.set(demoObj1.scale.x,demoObj1.scale.y,demoObj1.scale.z);
-      obj.position.set(demoObj1.position.x,demoObj1.position.y,demoObj1.position.z);
-      console.log("元素",obj)
-      data.scene.add(obj);
-    });
-  });
-  
+import {OBJLoader, MTLLoader} from 'three-obj-mtl-loader';
+import {CSS2DRenderer, CSS2DObject} from 'three-css2drender';
+//封裝Obj對象
+export const addObj2=(mesh,scene)=>{
+       //包含材质
+       new MTLLoader().setPath(mesh.path).load(mesh.mtl, materials => {
+        materials.preload();
+        new OBJLoader().setMaterials(materials).setPath(mesh.path).load(mesh.loadobj, obj => {
+          obj.scale.set(mesh.scale.x, mesh.scale.y, mesh.scale.z);
+          obj.position.set(mesh.position.x, mesh.position.y, mesh.position.z);
+          for(let item of mesh.spriteInfo){
+            addSprite(item.x,item.y,item.z,item.image,item.loc,item.text,obj,()=>{})
+          }
+          scene.add(obj);
+        });
+      });
 }
+
+export const addSprite=(x, y, z, image, loc, text, Mash, callback)=> {
+        //添加div标签
+        var biaozhudiv = document.createElement('div');
+        //添加图标标签
+        var img = document.createElement('img');
+        img.src = image;
+        img.style.marginLeft = loc + 'px';
+        biaozhudiv.className = 'lable';
+        //两者的执行顺序
+        biaozhudiv.textContent = text;
+        biaozhudiv.appendChild(img);
+        //标注的样式
+        biaozhudiv.id = 'biaozhu';
+        biaozhudiv.style.color = 'rgb(' + 0 + ',' + 0 + ',' + 0 + ')';
+        biaozhudiv.style.fontSize = 15 + 'px';
+        biaozhudiv.style.fontFamily = 'Georgia,serif';
+        biaozhudiv.style.cursor = 'pointer';
+        biaozhudiv.onclick = function () {
+          callback(Mash);
+        };
+        var biaozhuLabel = new CSS2DObject(biaozhudiv);
+        biaozhuLabel.position.set(x, y, z);
+        Mash.add(biaozhuLabel);
+        return Mash
+      }
+
+
 export const addObj=(x,y,z)=>{
   //1.定義一個幾何體
   var geometry = new THREE.Geometry();
